d3d698c9f0f97a1c55f5d910560f470a31f4a21f
Galitskiy Kirill <9keyoujob9@gmail.com>
2022-11-02 08:06:33 +0300

    Started first task

--- Homework/Task1/Program.cs
+++ Homework/Task1/Program.cs
@@ -8,3 +8,47 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9
 */
+
+int GetNumber (string message) {
+    int result = 0;
+    bool isCorrect = false;
+    Console.WriteLine(message);
+    while (!isCorrect) {
+        isCorrect = int.TryParse(Console.ReadLine(), out result);
+        if (!isCorrect) {
+            Console.WriteLine($"Ввели не число или оно слишком большое. Введите корректное число \n");
+        }
+    }
+    return result;
+}
+
+double[,] GetArray (int m, int n) {
+    Random random = new Random();
+    double[,] array = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = Math.Round(random.NextDouble()*1000, 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray (double[,] array) {
+    Console.WriteLine("Получили матрицу:\n");
+    int row = array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write("{0,7}", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+int columns = GetNumber("Введите количество столбцов: ");
+int row = GetNumber("Введите количество строк: ");
+
+PrintArray(GetArray(row, columns));
